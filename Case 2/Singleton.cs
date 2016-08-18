0066be54472eb0cf3a77ac94c68ad327029c7cda
Doctengineer <botaoxie88@gmail.com>
2016-08-18 14:45:29 +0800

    C#

---
+++ Case 2/Singleton.cs
@@ -0,0 +1,18 @@
+public sealed class Singleton1
+{
+    private Singleton1()
+    {
+    }
+
+    private static Singleton1 instance = null;
+    public static Singleton1 instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Singleton1();
+
+            return instance;
+        }
+    }
+}
